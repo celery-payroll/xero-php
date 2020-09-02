b6611ef46b27e7c302fb8be2e6fd97e4f46a8473
FΞLIX <felix@celerypayroll.com>
2020-09-02 09:55:55 -0400

    Refactors Overpayments and Credit Notes in Invoice Model
    # Conflicts:
    #	src/XeroPHP/Models/Accounting/Invoice.php

--- src/XeroPHP/Models/Accounting/Invoice.php
+++ src/XeroPHP/Models/Accounting/Invoice.php
@@ -327,13 +327,13 @@ class Invoice extends Remote\Model
             'HasAttachments' => [false, self::PROPERTY_TYPE_BOOLEAN, null, false, false],
             'Payments' => [false, self::PROPERTY_TYPE_OBJECT, 'Accounting\\Payment', true, false],
             'Prepayments' => [false, self::PROPERTY_TYPE_OBJECT, 'Accounting\\Prepayment', true, false],
-            'Overpayments' => [false, self::PROPERTY_TYPE_OBJECT, 'Accounting\\Overpayment', true, false],
+            'Overpayments' => [false, self::PROPERTY_TYPE_OBJECT, 'Accounting\\Overpayment\\Allocation', true, false],
             'AmountDue' => [false, self::PROPERTY_TYPE_FLOAT, null, false, false],
             'AmountPaid' => [false, self::PROPERTY_TYPE_FLOAT, null, false, false],
             'FullyPaidOnDate' => [false, self::PROPERTY_TYPE_DATE, '\\DateTimeInterface', false, false],
             'AmountCredited' => [false, self::PROPERTY_TYPE_FLOAT, null, false, false],
             'UpdatedDateUTC' => [false, self::PROPERTY_TYPE_TIMESTAMP, '\\DateTimeInterface', false, false],
-            'CreditNotes' => [false, self::PROPERTY_TYPE_OBJECT, 'Accounting\\CreditNote', true, false],
+            'CreditNotes' => [false, self::PROPERTY_TYPE_OBJECT, 'Accounting\\CreditNote\\Allocation', true, false],
         ];
     }
 
@@ -763,7 +763,7 @@ class Invoice extends Remote\Model
     }
 
     /**
-     * @return Overpayment[]|Remote\Collection
+     * @return Overpayment\Allocation[]|Remote\Collection
      */
     public function getOverpayments()
     {
@@ -811,7 +811,7 @@ class Invoice extends Remote\Model
     }
 
     /**
-     * @return CreditNote[]|Remote\Collection
+     * @return CreditNote\Allocation[]|Remote\Collection
      */
     public function getCreditNotes()
     {
